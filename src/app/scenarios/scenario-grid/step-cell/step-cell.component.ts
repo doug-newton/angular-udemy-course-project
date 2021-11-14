92db93c293333f4bf0c00b433a109d7592cdf223
Doug <27059467+doug-newton@users.noreply.github.com>
2021-11-14 12:43:43 +0200

    make cells editable

--- src/app/scenarios/scenario-grid/step-cell/step-cell.component.ts
+++ src/app/scenarios/scenario-grid/step-cell/step-cell.component.ts
@@ -4,16 +4,41 @@ import { Scenario } from '../../../scenario.model'
 @Component({
   selector: '[app-step-cell]',
   templateUrl: './step-cell.component.html',
-  styleUrls: ['./step-cell.component.css']
+  styleUrls: ['./step-cell.component.css'],
+  host: {
+    "(click)": "startEditing($event)"
+  }
 })
 export class StepCellComponent implements OnInit {
 
   @Input() scenario: Scenario
   @Input() stepColumn: string
 
+  stepContent: string = ''
+
+  editing: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  startEditing() {
+    if (!this.editing) {
+      if (this.stepColumn in this.scenario.steps) {
+        this.stepContent = this.scenario.steps[this.stepColumn]
+      }
+      this.editing = true;
+    }
+  }
+
+  save() {
+    if (this.editing) {
+      if (this.stepContent !== '') {
+        this.scenario.steps[this.stepColumn] = this.stepContent
+      }
+      this.editing = false;
+    }
   }
 
 }
