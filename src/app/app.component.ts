e262042be3e9eb95587927b4a88e192f3f657740
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 19:09:35 +0200

    fix styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ServerElement } from './server-element.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   serverElements: ServerElement[] = [];
