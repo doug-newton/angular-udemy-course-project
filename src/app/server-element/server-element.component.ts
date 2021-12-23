ceee8934c04f5aee823219e522fa88e8355a42eb
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 18:44:31 +0200

    Revert "use ViewEncapsulation.None to apply ServerElementComponent styles globally"
    This reverts commit f98ddead144572ca62033e919f3757d609998959.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ServerElement } from '../server-element.model';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.None // apply styles from this component globally (not inheritance)
-  //  encapsulation: ViewEncapsulation.ShadowDom // use native browser for default angular behaviour (not all support this)
-  //  encapsulation: ViewEncapsulation.Emulated // default angular behaviour
+  styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
 
