ed3dcbbce598fa4476f4deab43204836f1a0fec1
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 20:57:26 +0200

    implement game control

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
 
+  @Output() numberEmitted: EventEmitter<number> = new EventEmitter
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  interval
+  count: number = 0
+
+  onStartClicked() {
+    this.interval = setInterval(()=>{
+      this.numberEmitted.emit(++this.count)
+    }, 500)
+  }
+
+  onPauseClicked() {
+    clearInterval(this.interval)
   }
 
 }
