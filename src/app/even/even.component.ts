d704917276f99daf3c46633a517e50df66c8f940
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 21:05:23 +0200

    implement and use odd,even components

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-even',
+  selector: '[app-even]',
   templateUrl: './even.component.html',
   styleUrls: ['./even.component.css']
 })
--- src/app/odd/odd.component.ts
+++ src/app/odd/odd.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-odd',
+  selector: '[app-odd]',
   templateUrl: './odd.component.html',
   styleUrls: ['./odd.component.css']
 })
 export class OddComponent implements OnInit {
+
+  @Input() num: number
 
   constructor() { }
 
