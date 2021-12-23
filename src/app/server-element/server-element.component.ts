6c0dbe0ab34c60a4e491108e24bd5ffd326a432c
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 19:19:44 +0200

    note lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ServerElement } from '../server-element.model';
 
+/*
+
+Lifecycle Hooks
+
+ngOnChanges - called after a bound input property changes
+ngOnInit - called once the component is initialised, before being displayed/added to the DOM. runs after the constructor
+ngDoCheck - called every change detection cycle run: when angular checks, not only when something does actually need to change
+ngAfterContentInit - called after content (ng-content) has been projected into the view
+ngAfterContentChecked - called after ng-content has been checked
+ngAfterViewInit - called after the component's view (and child views) has been initialised
+ngAfterViewChecked - called after the view and child views have been checked
+ngOnDestroy - called once the component is about to be destroyed / removed from the DOM
+
+*/
+
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
