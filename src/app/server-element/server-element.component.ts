b75059d19378b260788e22e26dbb1f26a2d28d40
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-23 18:26:28 +0200

    extract and use server element component

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ServerElement } from '../server-element.model';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+
+  @Input() serverElement: ServerElement
 
   constructor() { }
 
