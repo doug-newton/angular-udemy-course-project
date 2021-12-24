d0e6df9dfd031b60c5577efbcb2eca252ec695c8
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 17:38:04 +0200

    add and display ingredients property in recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,25 @@ export class RecipeService {
     recipeSelect: EventEmitter<Recipe> = new EventEmitter
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg'),
-        new Recipe('Another Test Recipe', 'This is another test recipe', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg'),
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty schnitzel -- just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Crumbed Chicken', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+
+        new Recipe('Big Fat Burger',
+            'What else do you need to know?',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Beef Patty', 1)
+            ]
+        ),
+
     ]
 
     public getRecipes(): Recipe[] {
