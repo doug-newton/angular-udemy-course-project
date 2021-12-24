db629868cb27636f4ab7c0a38c49a15c83d0ca7f
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 12:19:44 +0200

    implement custom structural 'unless' directive

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+/*
+this is the opposite of an *ngIf
+*/
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
 
-  constructor() { }
+  //  'set' is TypeScript shorthand for setter of property
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef)
+    }
+    else {
+      this.vcRef.clear()
+    }
+  }
+
+  constructor(
+    /*
+    the star syntax will wrap the element in an <ng-template>.
+    therefore inject a TemplateRef instead of an ElementRef.
+    */
+    private templateRef: TemplateRef<any>,
+    /*
+    this is the location in the DOM where the <ng-template> will be rendered.
+    you can see this using the dev tools.
+    */
+    private vcRef: ViewContainerRef
+  ) { }
 
 }
