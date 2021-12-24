15b6e83d610f42db343917f8f7b04c5b2623a8e5
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 12:02:47 +0200

    use tuple for directive property binding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -5,24 +5,23 @@ import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/co
 })
 export class BetterHighlightDirective implements OnInit {
 
-  @Input() defaultColor: string = 'transparent'
-  @Input('appBetterHighlight') highlightColor: string = 'blue'
+  @Input('appBetterHighlight') colors: [string, string] = ['transparent','blue']
 
   constructor(
   ) { }
 
   ngOnInit(): void {
-    this.backgroundColor = this.defaultColor
+    this.backgroundColor = this.colors[0]
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
-    this.backgroundColor = this.highlightColor
+    this.backgroundColor = this.colors[1]
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
-    this.backgroundColor = this.defaultColor
+    this.backgroundColor = this.colors[0]
   }
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.colors[0]
 
 }
