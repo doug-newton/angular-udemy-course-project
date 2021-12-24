4e5e302631ff3fed615a64a7103446f85da4c134
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 12:44:25 +0200

    implement dropdown directive using nativeElement
    (my solution)

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,8 +1,24 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
+
+    constructor(
+        private elementRef: ElementRef,
+    ) {
+    }
+
+    toggled = false
+
+    @HostListener('click') onClick(eventData: Event) {
+        this.toggled = !this.toggled
+        if (this.toggled)
+            this.elementRef.nativeElement.classList.add('open')
+        else
+            this.elementRef.nativeElement.classList.remove('open')
+
+    }
 
 }
