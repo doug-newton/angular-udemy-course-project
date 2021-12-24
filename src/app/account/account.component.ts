717e3eacb0d096b607b007c8ad3a9268f5334e77
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 13:24:28 +0200

    Revert "use LoggingService naively"
    This reverts commit 3e482e044b2133c03d9b4bee3fd057f096c1a676.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
@@ -14,9 +13,6 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-
-    //  you should not do this
-    const service = new LoggingService()
-    service.logStatusChange(status)
+    console.log('A server status changed, new status: ' + status);
   }
 }
