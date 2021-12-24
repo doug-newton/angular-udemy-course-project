37558bd945fef4c5649d8186d80da67985efd979
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 14:02:16 +0200

    inject LoggingService into AccountsService

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,4 +1,23 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+/*
+NB - the @Injectable() decorator is for the receiving class of the injection, 
+not for the class to be injected.
+if a service will not have anything injected into it, then this is
+not necessary.
+
+however, it's recommended that you do use it regardless for future versions of angular
+*/
+
+@Injectable()
 export class AccountsService {
+
+  constructor(
+    private loggingService: LoggingService
+  ){
+  }
+
   accounts = [
     {
       name: 'Master Account',
@@ -18,10 +37,12 @@ export class AccountsService {
     this.accounts.push({
         name, status
     });
+    this.loggingService.logStatusChange(status)
   }
 
   updateStatus(id: number, status:string) {
     this.accounts[id].status = status
+    this.loggingService.logStatusChange(status)
   }
 
 }
