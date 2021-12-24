bafce7ea94d711fd01d74288cb95ffc815084883
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-24 10:02:49 +0200

    add second dummy recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,8 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeSelect: EventEmitter<Recipe> = new EventEmitter
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is a test recipe', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg')
+    new Recipe('A Test Recipe','This is a test recipe', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg'),
+    new Recipe('Another Test Recipe','This is another test recipe', 'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg'),
   ]
 
   constructor() { }
