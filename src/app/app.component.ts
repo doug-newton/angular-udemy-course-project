08215d1ee400f11dba29671ac4df00f4a9647c1a
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 20:43:32 +0200

    set up reactive forms subproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,46 +6,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('myForm') form: NgForm
-  answer: string = ''
-  genders: string[] = ['male', 'female']
-  submitted = false
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    answer: '',
-    gender: ''
-  }
-
-  onSetValue() {
-    this.form.setValue({
-      userData: {
-        username: 'Superuser',
-        email: ''
-      },
-      secret: 'teacher',
-      answer: '',
-      gender: 'male'
-    })
-  }
-
-  onPatchValue() {
-    this.form.form.patchValue({
-      userData: {
-        username: 'Superuser'
-      }
-    })
-  }
-
-  onSubmit() {
-    this.submitted = true
-    this.user.username = this.form.value.userData.username
-    this.user.email = this.form.value.userData.email
-    this.user.secret = this.form.value.secret
-    this.user.answer = this.form.value.answer
-    this.user.gender = this.form.value.gender
-    this.form.reset()
-  }
+  genders = ['male', 'female'];
 }
