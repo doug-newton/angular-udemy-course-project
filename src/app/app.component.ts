fb359fa4102f8d81c21012ef58113ace5cce34e9
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 23:52:22 +0200

    create FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup
+
+  ngOnInit(): void {
+      this.projectForm = new FormGroup({
+        name: new FormControl(null),
+        email: new FormControl(null),
+        status: new FormControl(null)
+      })
+  }
+
 }
