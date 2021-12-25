b423d17210cc1fa8f4453139cb127b5a350a5d7b
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 14:35:39 +0200

    react to observer complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,17 +17,25 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0
       setInterval(()=>{
         observer.next(count++)
+        if (count == 2) {
+          observer.complete()
+        }
         if (count > 3) {
           observer.error(new Error("count is more than 3"))
         }
       }, 1000)
     })
 
-    this.firstSubscription = customObservable.subscribe(count => {
-      console.log(count)
-    },
-    error => {
-      alert(error.message)
+    this.firstSubscription = customObservable.subscribe({
+      next(count) {
+        console.log(count)
+      },
+      error(error) {
+        alert(error.message)
+      },
+      complete() {
+        console.log('completed')
+      }
     })
   }
 
