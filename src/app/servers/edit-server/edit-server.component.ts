bcb22eb278d4aec0e9ac3b7404fe7078a0f20c20
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 10:58:10 +0200

    implement CanComponentDeactivate on EditServer

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ServersService } from '../servers.service';
+import { CanComponentDeactive } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactive {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -28,6 +30,18 @@ export class EditServerComponent implements OnInit {
     this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+    if (!this.allowEdit) return true
+    if (
+      (this.serverName !== this.server.name || this.serverStatus !== this.server.status)
+      &&
+      !this.changesSaved
+    ) {
+      return confirm('do you want to discard the changes?')
+    }
+    else return true
   }
 
   onUpdateServer() {
