0286c9808f518e01e019e262bc7af5c4367b2dac
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 11:53:47 +0200

    resolve dynamic server with resolve: {server: ServerResolver}

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -18,10 +18,8 @@ export class ServerComponent implements OnInit {
     ) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id']
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe(params => {
-      this.server = this.serversService.getServer(+params['id']);
+    this.route.data.subscribe(data => {
+      this.server = data['server']
     })
   }
 
