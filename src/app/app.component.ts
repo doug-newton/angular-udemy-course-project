45c0ea8b8db005ad95cb31b900b01c6c116027c6
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-26 00:17:27 +0200

    create and use async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
       this.projectForm = new FormGroup({
-        name: new FormControl(null, [Validators.required, this.nameIsNotTestValidator]),
+        name: new FormControl(null, Validators.required, this.nameIsNotTestAsyncValidator),
         email: new FormControl(null, [Validators.required, Validators.email]),
         status: new FormControl(null, Validators.required)
       })
@@ -23,6 +24,18 @@ export class AppComponent implements OnInit {
       return { nameIsTest: true }
     }
     else return null
+  }
+
+  nameIsNotTestAsyncValidator(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        if (control.value === 'Test') {
+          resolve({nameIsTest: true})
+        } else {
+          resolve(null)
+        }
+      }, 500)
+    })
   }
 
   onSubmit() {
