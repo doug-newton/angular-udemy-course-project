c11bd78856fc686277628d83eb7c59f8e0cd318f
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 20:32:17 +0200

    use @ViewChild() and log ngForm on submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('signupForm') signupForm: NgForm
+
   onSubmit() {
-    console.log('submitted')
+    console.log(this.signupForm)
   }
 }
