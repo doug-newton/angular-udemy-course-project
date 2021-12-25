3117bb1139eb9b77add6869ca689fe69686a756d
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-26 00:10:15 +0200

    create and use custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,17 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
       this.projectForm = new FormGroup({
-        name: new FormControl(null, Validators.required),
+        name: new FormControl(null, [Validators.required, this.nameIsNotTestValidator]),
         email: new FormControl(null, [Validators.required, Validators.email]),
         status: new FormControl(null, Validators.required)
       })
+  }
+
+  nameIsNotTestValidator(control: FormControl): {[errorCode:string]:boolean} {
+    if (control.value === 'Test') {
+      return { nameIsTest: true }
+    }
+    else return null
   }
 
   onSubmit() {
