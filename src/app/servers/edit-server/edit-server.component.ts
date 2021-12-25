18f1c9b5b1c5b8b7ae342a724a7a7de205f323b0
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 10:45:07 +0200

    edit correct server and nav back on update

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -12,23 +12,28 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
   allowEdit = false
+  changesSaved = false
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
     ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(queryParams=>{
       this.allowEdit = queryParams['allowEdit'] === '1' ? true : false
     })
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id']
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 
 }
