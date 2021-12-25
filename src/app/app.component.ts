23ec20c5c58a58bce8132a793c4075eb8676fcb7
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 12:23:57 +0200

    add routing and routerLinks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy-course-project';
-  
-  currentPage: string = 'recipes'
-
-  onSwitchPage($event: string) {
-    this.currentPage = $event
-  }
 }
