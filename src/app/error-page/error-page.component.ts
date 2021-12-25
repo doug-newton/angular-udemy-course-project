4e06619fd1470d9854d63eec20df9c3aafe4eef2
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-25 11:40:10 +0200

    pass static data to route

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -9,9 +10,15 @@ export class ErrorPageComponent implements OnInit {
 
 errorMessage: string
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.errorMessage = this.route.snapshot.data['message']
+    this.route.data.subscribe(data =>{
+      this.errorMessage = data['message']
+    })
   }
 
 }
