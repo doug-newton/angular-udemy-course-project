3b53c573a76b9f9671f2f4a5a2e3a8b3f8cf7fcb
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-26 09:29:17 +0200

    deleteRecipe in service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -45,6 +45,11 @@ export class RecipeService {
         this.recipesChanged.next(this.recipes.slice())
     }
 
+    deleteRecipe(index:number) {
+        this.recipes.splice(index, 1)
+        this.recipesChanged.next(this.recipes.slice())
+    }
+
     getRecipe(id: number): Recipe {
         return this.recipes[id]
     }
