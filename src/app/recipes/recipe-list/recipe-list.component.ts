8183606c577224709075765673d9e70c1c1e7701
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-26 09:20:17 +0200

    add and subscribe to recipesChanged Subject

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,7 +9,7 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[]
 
@@ -18,8 +19,17 @@ export class RecipeListComponent implements OnInit {
     private route: ActivatedRoute
   ) { }
 
+  subscription: Subscription
+
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes()
+    this.subscription = this.recipeService.recipesChanged.subscribe(recipes => {
+      this.recipes = recipes
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe()
   }
 
   onCreateNewRecipe() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+
+    recipesChanged: Subject<Recipe[]> = new Subject
 
     constructor(
         private shoppingListService: ShoppingListService
@@ -34,10 +37,12 @@ export class RecipeService {
 
     addRecipe(recipe: Recipe) {
         this.recipes.push(recipe)
+        this.recipesChanged.next(this.recipes.slice())
     }
 
     updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe
+        this.recipesChanged.next(this.recipes.slice())
     }
 
     getRecipe(id: number): Recipe {
