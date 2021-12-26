d0b3fea37ce903ebe394685146f2eba2f0c4f8cb
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-26 10:43:46 +0200

    allow filter value as input for filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  filterValue: string = ''
+
   servers = [
     {
       instanceType: 'medium',
