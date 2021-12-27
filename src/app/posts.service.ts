b6e88f25ef25c87d3710d573178d5289387b150e
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-27 06:42:38 +0200

    create posts service to handle http requests

---
+++ src/app/posts.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Post } from "./post.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsService {
+
+    constructor(
+        private http: HttpClient
+    ){}
+
+    createPost(title: string, content: string): Observable<any> {
+        const postData: Post = {
+            title, content
+        }
+        return this.http.post('http://localhost:8080/api/posts', postData)
+    }
+
+    fetchPosts(): Observable<Post[]> {
+        return this.http.get<Post[]>('http://localhost:8080/api/posts')
+    }
+}
