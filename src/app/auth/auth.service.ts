1cf4869835ade81bc99d72c5695d20bd5741dd24
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2022-01-02 13:08:50 +0200

    return Observable<object> from auth service signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn:'root'
@@ -10,8 +11,8 @@ export class AuthService {
         private http: HttpClient
     ) { }
 
-    signup(username: string, password: string) {
-        return this.http.post('http://localhost:8080/api/users/signup', {
+    signup(username: string, password: string): Observable<object> {
+        return this.http.post<object>('http://localhost:8080/api/users/signup', {
             username, password
         })
     }
